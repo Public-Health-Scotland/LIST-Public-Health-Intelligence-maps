d93be3e2badcfb6e59b4154af4e68950cde1d4e6
jamesk-PHS <james.kilgour2@phs.scot>
2025-03-14 12:21:31 +0000

    trying to expand on this but it's not *quite* working

--- Scripts/Greenfield-brownfield mapping test script.R
+++ Scripts/Greenfield-brownfield mapping test script.R
@@ -51,19 +51,45 @@ path <- "/conf/LIST_analytics/Lanarkshire/Documentation/James/Mapping for Ruth/m
 
 greenbelt <- read_sf(paste0(path, "Green_Belt_-_Scotland/pub_grnblt.shp")) %>% 
   filter(str_detect(local_auth, "Lanarkshire")) %>%  # Filtering for necessary data only
-  st_transform(4326)
-
-greenbelt <- greenbelt[2] %>% # Selecting key variables only
+  st_transform(4326) %>% 
+  dplyr::select(local_auth) %>% # Selecting key variables only
   mutate(colour = "green") # Adding colour variable for mapping
 
 brownfield <- read_sf(paste0(path, "Vacant_and_Derelict_Land_-_Scotland/pub_vdlPolygon.shp")) %>% 
   filter(str_detect(local_auth, "Lanarkshire")) %>%  # Filtering for necessary data only
-  st_transform(4326)
-
-brownfield <- brownfield[1] %>% 
+  st_transform(4326) %>% 
+  dplyr::select(local_auth)%>% 
   mutate(colour = "brown")
 
 rm(path)
+
+
+
+population_density <- read_sf("/conf/linkage/output//lookups/Unicode/Geography/Shapefiles/SG_DataZoneCent_2011/SG_DataZone_Cent_2011.shp") %>% 
+  janitor::clean_names() %>% 
+  dplyr::select(data_zone, tot_pop2011) %>% 
+  st_transform(4326) %>% 
+  
+  left_join(
+  phsopendata::get_resource("d6e500c4-c1f2-4507-979a-e18855efd7a4") %>% 
+    janitor::clean_names() %>% 
+    filter(hscp %in% c("S37000021", "S37000028", "S37000035")) %>% 
+    mutate(hscp = if_else(hscp == "S37000028", "South Lanarkshire", "North Lanarkshire")) %>% 
+    dplyr::select(data_zone, locality = sub_hscp_name, hscp),
+  
+  by = "data_zone") %>%  # end of left_join() call
+  
+  na.omit() %>% 
+  
+  left_join(read_rds("/conf/linkage/output//lookups/Unicode/Geography/Urban Rural Classification/DataZone2011_urban_rural_2020v2.rds") %>% 
+              janitor::clean_names() %>% 
+              dplyr::select(data_zone = datazone2011, ur8_2020_name),
+            
+            by = "data_zone") 
+
+pal_1 <- colorNumeric("magma", population_density$tot_pop2011)
+pal_2 <- colorFactor("magma", population_density$ur8_2020_name)
+
 
 ### 3 Section Heading ----
 
@@ -72,15 +98,37 @@ leaflet() %>%
   addPolygons(data = greenbelt,
               fillColor = "green",
               weight = 1,
-              color = "green") %>% 
+              color = "green",
+              group = "Land use") %>% 
   addPolygons(data = brownfield,
               fillColor = "brown",
               weight = 1,
-              color = "brown") %>% 
+              color = "brown",
+              group = "Land use") %>% 
+  addPolygons(data = population_density,
+              fillColor = ~pal_1(tot_pop2011),
+              weight = 1,
+              color = "black",
+              group = "Population estimates") %>%
+  addPolygons(data = population_density,
+              fillColor = ~pal_2(ur8_2020_name),
+              weight = 1,
+              color = "black",
+              group = "Urbanity") %>%
   addLegend(position = "bottomleft", 
             title = "Site type",
             colors = c("green", "brown"), 
-            labels = c("Greenfield", "Brownfield"))
+            labels = c("Greenfield", "Brownfield"),
+            group = "Land use") %>%
+  addLegend(pal = pal,
+            position = "bottomleft", 
+            title = "Urbanity",
+            labels = population_density$ur8_2020_name,
+            group = "Urbanity") %>%
+  addLayersControl(
+    overlayGroups = c("Land use", "Population estiamtes", "Urbanity"),
+    options = layersControlOptions(collapsed = FALSE)
+  )
 
 
 ### END OF SCRIPT ###
