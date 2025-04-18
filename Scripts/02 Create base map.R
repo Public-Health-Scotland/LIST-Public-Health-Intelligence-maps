d098257ed462228921a9a6d1dd694e0da03ddaab
James <james.kilgour2@phs.scot>
2025-04-18 15:07:59 +0000

    Added rail network from https://find.data.gov.scot/datasets/32646

--- Scripts/02 Create base map.R
+++ Scripts/02 Create base map.R
@@ -5,12 +5,16 @@ library(sf) # Reading shape files
 
 
 
-# Read in cycling network shapefiles --------------------------------------
+# Read in public transport network shapefiles --------------------------------------
 
-
+# Cycling data
 cycling_network <- read_sf("Cycling_Network_-_Scotland/pub_cycnt.shp") %>% 
-  # transform the polygons
-  st_transform(4326)
+  st_transform(4326) # transform polygons to long/lat system
+
+rail_network <- read_sf("UK_Railways/Railway.shp") %>% 
+  filter(FILE_NAME == "gb_north") %>% 
+  st_set_crs(27700) %>% # Assign coordinate ref. system to data
+  st_transform(4326) # Convert to long/lat system
 
 
 
@@ -23,11 +27,15 @@ All_Services_Locations %>%
   leaflet()  %>% 
   addProviderTiles(providers$Stadia.AlidadeSmooth) %>% 
   # Add train network
-  addProviderTiles(providers$OpenRailwayMap) %>% 
+  # Add train network
+  addPolylines(data = rail_network,
+               weight = 2,
+               opacity = 0.25,
+               color = "red") %>% 
   # Add cycling network
   addPolylines(data = cycling_network,
-              weight = 1,
-              opacity = 0.75,
+              weight = 2,
+              opacity = 0.25,
               color = "purple") %>% 
   # Markers for sites
   addCircleMarkers(
