f865a38482dfc90893ec1c41e2438b3542d791cc
James <james.kilgour2@phs.scot>
2025-04-25 11:51:50 +0000

    Tweaking colours to match AwesomeIcons hex codes and adding in sites as unique groups for full layer control and interactivity.

--- Scripts/02 Create base map.R
+++ Scripts/02 Create base map.R
@@ -19,7 +19,7 @@ rail_network <- read_sf("UK_Railways/Railway.shp") %>%
 
 
 pal <- colorFactor(
-  palette = c('beige', 'pink', 'blue', 'darkred', 'green', "red", "darkgreen"),
+  palette = c('#f0c28d', '#f88de6', '#2596be', 'darkred', '#6ea728', "red", "#6f7e28"),
   domain = All_Services_Locations$service_type
 )
 
@@ -51,6 +51,7 @@ logos <- awesomeIconList(
 )
   
 
+# Create transport links baseline map
 base_map <- All_Services_Locations %>% 
   # Load map
   leaflet()  %>%
@@ -61,21 +62,46 @@ base_map <- All_Services_Locations %>%
   addPolylines(data = rail_network,
                weight = 2,
                opacity = 0.5,
-               color = "darkgreen") %>% 
+               color = "darkgreen",
+               group = "Rail network") %>% 
   # Add cycling network
   addPolylines(data = cycling_network,
-              weight = 2,
-              opacity = 0.25,
-              color = "purple") %>% 
-  # Markers for sites
-  #addCircleMarkers(
-  #  lng = All_Services_Locations$longitude, lat = All_Services_Locations$latitude, group = "Sites",
-  #  radius = 3.5, weight = 1, opacity = 1, fillOpacity = 1, fillColor = ~pal(service_type), color = ~pal(service_type)) %>% 
-  addAwesomeMarkers(icon = ~logos[service_type], # lookup from list based on ticker
-                    label = ~service_type) %>% 
+               weight = 2,
+               opacity = 0.25,
+               color = "purple",
+               group = "Cycle network")
+
+
+
+
+# Iterate over the service types and add each to the map as a unique group:
+unique(All_Services_Locations$service_type) %>%
+  purrr::walk( function(x) {
+    
+    data <- All_Services_Locations %>% 
+      filter(service_type == x)
+    
+    base_map <<- base_map %>%
+      addAwesomeMarkers(data=data,
+                        icon = ~logos[service_type], # lookup from list based on ticker
+                        label = ~name,
+                        clusterOptions = markerClusterOptions(minSize = 20, maxClusterRadius = 1, freezeAtZoom = 20),
+                        group = ~x)
+  })
+
+
+# Add final layer controls based on the unique service groups 
+base_map <- base_map %>% 
+  addLayersControl(
+    position = "topright",
+    overlayGroups = unique(All_Services_Locations$service_type),
+    baseGroups = c("Cycle network", "Rail network"),
+    # set collapsed = FALSE so that controls always displayed
+    options = layersControlOptions(collapsed = FALSE)
+  ) %>% 
   addLegend(data = All_Services_Locations,
             position = "bottomleft", 
             title = "Site Type",
             pal = pal, 
-            values = ~service_type) 
+            values = ~service_type)
 
